9967d71cbe777e10d218eb75db7f2a1e64316591
Fredmanzi <manzifred13@gmail.com>
2022-09-14 19:14:55 +0200

    modifying element in dom

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+var headTitle = document.getElementById('heading-title');
+
+// headTitle.textContent = 'hello';
+// headTitle.innerText = 'kigurube';
+// headTitle.innerHTML = '<h3>mukuru wazo</h3>'
+// headTitle.style.borderBottom = 'solid 3px black';
+var listItem = document.getElementsByClassName('list_item');
+listItem[1].textContent = 'pussy';
+
+console.log(listItem);
